d24e36841e98bd09aa2da9af45967a46f605282f
Omri Attal <omriattal@gmail.com>
2021-03-24 17:00:50 +0200

    added baseline calculation

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,15 @@
 import pandas as pd
+from sklearn.metrics import f1_score
 df = pd.read_csv('./Data/train/17-18_allgames.csv')
-print(df.head())
+df['Home Win'] = df['Home Points'] > df['Visitor Points']
+labels = df['Home Win'].values
+print(labels)
+shape = labels.shape
+labels_hometeamwins = [1] * len(labels)
+percentage = df['Home Win'].sum() / len(labels)
+print(percentage)
+print(f1_score(labels,labels_hometeamwins,pos_label=None, average='weighted'))
+
+
+
+
