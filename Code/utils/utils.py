2f4704f938021d4f6cbfca57a087ad30b55a28a7
Omri Attal <omriattal@gmail.com>
2021-03-24 19:08:56 +0200

    merge sort

---
+++ Code/utils/utils.py
@@ -0,0 +1,58 @@
+
+def symbol_to_team(symbol):
+    if symbol == "CLE":
+        return "Cleveland Cavaliers"
+    if symbol == "MIL":
+        return "Milwaukee Bucks"
+    if symbol == "TOR":
+        return "Toronto Raptors"
+    if symbol == "DEN":
+        return "Denver Nuggets"
+    if symbol == "HOU":
+        return "Houston Rockets"
+    if symbol == "POR":
+        return "Portland Trail Blazers"
+    if symbol == "PHI":
+        return "Philadelphia 76ers"
+    if symbol == "UTA":
+        return "Utah Jazz"
+    if symbol == "BOS":
+        return "Boston Celtics"
+    if symbol == "OKC":
+        return "Oklahoma City Thunder"
+    if symbol == "IND":
+        return "Indiana Pacers"
+    if symbol == "LAC":
+        return "Los Angeles Clippers"
+    if symbol == "SAS":
+        return "San Antonio Spurs"
+    if symbol == "BKN":
+        return "Brooklyn Nets"
+    if symbol == "ORL":
+        return "Orlando Magic"
+    if symbol == "DET":
+        return "Detroit Pistons"
+    if symbol == "CHO":
+        return "Charlotte Hornets"
+    if symbol == "MIA":
+        return "Miami Heat"
+    if symbol == "SAC":
+        return "Sacramento Kings"
+    if symbol == "LAL":
+        return "Los Angeles Lakers"
+    if symbol == "MIN":
+        return "Minnesota Timberwolves"
+    if symbol == "DAL":
+        return "Dallas Mavericks"
+    if symbol == "NOP":
+        return "New Orleans Pelicans"
+    if symbol == "WAS":
+        return "Washington Wizards"
+    if symbol == "ATL":
+        return "Atlanta Hawks"
+    if symbol == "CHI":
+        return "Chicago Bulls"
+    if symbol == "PHO":
+        return "Phoenix Suns"
+    if symbol == "NYC":
+        return "New York Knicks"
